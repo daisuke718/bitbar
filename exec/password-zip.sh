b3c2eac02e9b0d400bbd3b884f11707de263d9ad
Daisuke HIROMITSU <hiromitsu.daisuke@gmail.com>
2018-03-26 18:41:50 +0900

    パスワードを指定できるようにした

--- exec/password-zip.sh
+++ exec/password-zip.sh
@@ -4,7 +4,10 @@ read -p 'zip target path: ' target
 workdir=`dirname $target`
 filename=`basename $target`
 zipfile=${filename%.*}.zip
-password=`pwgen -sy 16 1`
+default_password=`pwgen -sy 16 1`
+printf "password[\e[35m$default_password\e[m]: "
+read password
+password=${password:-$default_password}
 pushd $workdir > /dev/null
 zip -e --password=$password $zipfile $filename > /dev/null
 printf "password : \e[31m$password\e[m\n"
